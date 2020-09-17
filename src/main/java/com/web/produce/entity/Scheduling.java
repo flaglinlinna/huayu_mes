b66b9accc0993d827d43adca721ebbf8833b75e0
shenxuwei <1224562835@qq.com>
2020-09-17 10:54:14 +0800

    sxw-排产导入模块添加列表和导出

---
+++ src/main/java/com/web/produce/entity/Scheduling.java
@@ -0,0 +1,339 @@
+package com.web.produce.entity;
+
+import com.app.base.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.web.po.entity.Interfaces;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 排产信息
+ */
+@Entity(name = "Scheduling")
+@Table(name= Scheduling.TABLE_NAME)
+@DynamicUpdate
+public class Scheduling extends BaseEntity {
+    private static final long serialVersionUID = -5951531333314901264L;
+    public static final String TABLE_NAME = "produce_scheduling";
+
+    /**
+     * 部门ID
+     */
+    @ApiModelProperty(name = "bsDepartId", value = "部门ID")
+    @Column
+    protected Long bsDepartId;
+
+    /**
+     * 生产日期
+     */
+    @ApiModelProperty(name = "bsProduceTime", value = "生产日期")
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+08:00")
+    protected Date bsProduceTime;
+
+    /**
+     * 班次
+     */
+    @ApiModelProperty(name = "bsShift", value = "班次")
+    @Column(length = 50)
+    protected String bsShift;
+
+    /**
+     * 客户
+     */
+    @ApiModelProperty(name = "bsCustomer", value = "客户")
+    @Column(length = 50)
+    protected String bsCustomer;
+
+    /**
+     * 线别
+     */
+    @ApiModelProperty(name = "bsLine", value = "线别")
+    @Column(length = 50)
+    protected String bsLine;
+
+    /**
+     * 工单类别
+     */
+    @ApiModelProperty(name = "bsOrderType", value = "工单类别")
+    @Column(length = 50)
+    protected String bsOrderType;
+
+    /**
+     * 制令单号
+     */
+    @ApiModelProperty(name = "bsUniqueOrderNo", value = "制令单号")
+    @Column
+    protected String bsUniqueOrderNo;
+
+    /**
+     * 工单号
+     */
+    @ApiModelProperty(name = "bsOrderNo", value = "工单号")
+    @Column(length = 100)
+    protected String bsOrderNo;
+
+    /**
+     * 生产状态
+     */
+    @ApiModelProperty(name = "bsStatus", value = "生产状态")
+    @Column(length = 50)
+    protected String bsStatus;
+
+    /**
+     * 物料ID
+     */
+    @ApiModelProperty(name = "bsMtrialId", value = "物料ID")
+    @Column
+    protected Long bsMtrialId;
+
+    /**
+     * 加工工艺ID（工序ID）
+     */
+    @ApiModelProperty(name = "bsProcId", value = "加工工艺ID（工序ID）")
+    @Column
+    protected Long bsProcId;
+
+    /**
+     * 工单残
+     */
+    @ApiModelProperty(name = "bsRestNum", value = "工单残")
+    @Column
+    protected Integer bsRestNum;
+
+    /**
+     * 计划生产数量
+     */
+    @ApiModelProperty(name = "bsPlanNum", value = "计划生产数量")
+    @Column
+    protected Integer bsPlanNum;
+
+    /**
+     * 用人量
+     */
+    @ApiModelProperty(name = "bsPeopleNum", value = "用人量")
+    @Column
+    protected Integer bsPeopleNum;
+
+    /**
+     * 产能
+     */
+    @ApiModelProperty(name = "bsCapacityNum", value = "产能")
+    @Column
+    protected Integer bsCapacityNum;
+
+    /**
+     * 预计工时(H/人)
+     */
+    @ApiModelProperty(name = "bsPlanHours", value = "预计工时(H/人)")
+    @Column
+    protected String bsPlanHours;
+
+    /**
+     * 实际生产数量
+     */
+    @ApiModelProperty(name = "bsActualNum", value = "实际生产数量")
+    @Column
+    protected Integer bsActualNum;
+
+    /**
+     * 实际工时(H/人)
+     */
+    @ApiModelProperty(name = "bsActualHours", value = "实际工时(H/人)")
+    @Column
+    protected String bsActualHours;
+
+    /**
+     * 计划金额
+     */
+    @ApiModelProperty(name = "bsPlanPrice", value = "计划金额")
+    @Column(precision = 24, scale = 5)
+    protected BigDecimal bsPlanPrice;
+
+    /**
+     * 实际金额
+     */
+    @ApiModelProperty(name = "bsActualPrice", value = "实际金额")
+    @Column(precision = 24, scale = 5)
+    protected BigDecimal bsActualPrice;
+
+    /**
+     * 备注
+     */
+    @ApiModelProperty(name = "bsRemark", value = "备注")
+    @Column(length = 500)
+    protected String bsRemark;
+
+    public Long getBsDepartId() {
+        return bsDepartId;
+    }
+
+    public void setBsDepartId(Long bsDepartId) {
+        this.bsDepartId = bsDepartId;
+    }
+
+    public Date getBsProduceTime() {
+        return bsProduceTime;
+    }
+
+    public void setBsProduceTime(Date bsProduceTime) {
+        this.bsProduceTime = bsProduceTime;
+    }
+
+    public String getBsShift() {
+        return bsShift;
+    }
+
+    public void setBsShift(String bsShift) {
+        this.bsShift = bsShift;
+    }
+
+    public String getBsCustomer() {
+        return bsCustomer;
+    }
+
+    public void setBsCustomer(String bsCustomer) {
+        this.bsCustomer = bsCustomer;
+    }
+
+    public String getBsLine() {
+        return bsLine;
+    }
+
+    public void setBsLine(String bsLine) {
+        this.bsLine = bsLine;
+    }
+
+    public String getBsOrderType() {
+        return bsOrderType;
+    }
+
+    public void setBsOrderType(String bsOrderType) {
+        this.bsOrderType = bsOrderType;
+    }
+
+    public String getBsUniqueOrderNo() {
+        return bsUniqueOrderNo;
+    }
+
+    public void setBsUniqueOrderNo(String bsUniqueOrderNo) {
+        this.bsUniqueOrderNo = bsUniqueOrderNo;
+    }
+
+    public String getBsOrderNo() {
+        return bsOrderNo;
+    }
+
+    public void setBsOrderNo(String bsOrderNo) {
+        this.bsOrderNo = bsOrderNo;
+    }
+
+    public String getBsStatus() {
+        return bsStatus;
+    }
+
+    public void setBsStatus(String bsStatus) {
+        this.bsStatus = bsStatus;
+    }
+
+    public Long getBsMtrialId() {
+        return bsMtrialId;
+    }
+
+    public void setBsMtrialId(Long bsMtrialId) {
+        this.bsMtrialId = bsMtrialId;
+    }
+
+    public Long getBsProcId() {
+        return bsProcId;
+    }
+
+    public void setBsProcId(Long bsProcId) {
+        this.bsProcId = bsProcId;
+    }
+
+    public Integer getBsRestNum() {
+        return bsRestNum;
+    }
+
+    public void setBsRestNum(Integer bsRestNum) {
+        this.bsRestNum = bsRestNum;
+    }
+
+    public Integer getBsPlanNum() {
+        return bsPlanNum;
+    }
+
+    public void setBsPlanNum(Integer bsPlanNum) {
+        this.bsPlanNum = bsPlanNum;
+    }
+
+    public Integer getBsPeopleNum() {
+        return bsPeopleNum;
+    }
+
+    public void setBsPeopleNum(Integer bsPeopleNum) {
+        this.bsPeopleNum = bsPeopleNum;
+    }
+
+    public Integer getBsCapacityNum() {
+        return bsCapacityNum;
+    }
+
+    public void setBsCapacityNum(Integer bsCapacityNum) {
+        this.bsCapacityNum = bsCapacityNum;
+    }
+
+    public String getBsPlanHours() {
+        return bsPlanHours;
+    }
+
+    public void setBsPlanHours(String bsPlanHours) {
+        this.bsPlanHours = bsPlanHours;
+    }
+
+    public Integer getBsActualNum() {
+        return bsActualNum;
+    }
+
+    public void setBsActualNum(Integer bsActualNum) {
+        this.bsActualNum = bsActualNum;
+    }
+
+    public String getBsActualHours() {
+        return bsActualHours;
+    }
+
+    public void setBsActualHours(String bsActualHours) {
+        this.bsActualHours = bsActualHours;
+    }
+
+    public BigDecimal getBsPlanPrice() {
+        return bsPlanPrice;
+    }
+
+    public void setBsPlanPrice(BigDecimal bsPlanPrice) {
+        this.bsPlanPrice = bsPlanPrice;
+    }
+
+    public BigDecimal getBsActualPrice() {
+        return bsActualPrice;
+    }
+
+    public void setBsActualPrice(BigDecimal bsActualPrice) {
+        this.bsActualPrice = bsActualPrice;
+    }
+
+    public String getBsRemark() {
+        return bsRemark;
+    }
+
+    public void setBsRemark(String bsRemark) {
+        this.bsRemark = bsRemark;
+    }
+}
---
+++ src/main/resources/static/js/web/produce/scheduling.js
@@ -0,0 +1,88 @@
+/**
+ * 接口管理
+ */
+var pageCurr;
+$(function () {
+    layui.use(['form', 'table', 'laydate'], function(){
+        var table = layui.table
+            ,form = layui.form
+            ,laydate = layui.laydate;
+
+        tableIns=table.render({
+            elem: '#iList'
+            ,url:context+'/scheduling/getList'
+            ,method: 'get' //默认：get请求
+            ,cellMinWidth: 80
+            ,page: true,
+            request: {
+                pageName: 'page' //页码的参数名称，默认：page
+                ,limitName: 'rows' //每页数据量的参数名，默认：limit
+            },
+            parseData: function (res) {
+                // 可进行数据操作
+                return {
+                    "count": res.data.total,
+                    "msg":res.msg,
+                    "data":res.data.rows,
+                    "code": res.status //code值为200表示成功
+                }
+            },
+            cols: [[
+                {type:'numbers'}
+                // ,{field:'id', title:'ID', width:80, unresize:true, sort:true}
+                ,{field:'bsCustomer', title:'客户', width:120}
+                ,{field:'bsLine', title:'线别', width:120}
+                ,{field:'bsOrderNo', title:'工单号', width:120}
+                ,{field:'bsMtrialId', title:'物料编码', width:120}
+                ,{field:'bsMtrialId', title:'物料描述', width:120}
+                ,{field:'bsProcId', title:'加工工艺', width:120}
+                ,{field:'bsRestNum', title:'工单残', width:120}
+                ,{field:'bsPlanNum', title:'计划生产数量', width:120}
+                ,{field:'bsPeopleNum', title:'用人量', width:120}
+                ,{field:'bsCapacityNum', title:'产能', width:120}
+                ,{field:'bsPlanHours', title:'预计工时(H/人)', width:120}
+                ,{field:'bsActualNum', title:'实际生产数量', width:120}
+                ,{field:'bsActualHours', title:'实际工时(H/人)', width:120}
+                ,{field:'bsPlanPrice', title:'计划金额', width:120}
+                ,{field:'bsActualPrice', title:'实际生产金额 ', width:120}
+                ,{field:'bsRemark', title:'备注', width:120}
+                // ,{field:'bsStatus', title:'状态',width:95,align:'center',templet:'#statusTpl'}
+                // ,{field:'modifiedTime', title: '更新时间', width:150}
+                // ,{field:'createdTime', title: '添加时间', width:150}
+                ,{fixed:'right', title:'操作', width:200, align:'center', toolbar:'#optBar'}
+            ]]
+            ,done: function(res, curr, count){
+                //如果是异步请求数据方式，res即为你接口返回的信息。
+                //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+                //console.log(res);
+                //得到当前页码
+                //console.log(curr);
+                //得到数据总量
+                //console.log(count);
+                pageCurr=curr;
+            }
+        });
+
+        //监听搜索框
+        form.on('submit(search)', function(data){
+            //重新加载table
+            load(data);
+            return false;
+        });
+
+
+    });
+});
+
+//重新加载表格（搜索）
+function load(obj){
+    //重新加载table
+    tableIns.reload({
+        where: {
+            keyword:obj.field.keyword
+        }
+        , page: {
+            curr: pageCurr //从当前页码开始
+        }
+    });
+}
