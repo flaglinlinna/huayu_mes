2623ca13b19322fb0ea662fcfa5272a9708ef376
fuyuanxiu <fuyuanxiu@126.com>
2020-09-18 10:55:03 +0800

    fyx-1

--- src/main/java/com/Swagger2.java
+++ src/main/java/com/Swagger2.java
@@ -21,7 +21,7 @@ public class Swagger2 {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com"))
+				.apis(RequestHandlerSelectors.basePackage("com.web"))
 				.paths(PathSelectors.any())
 				.build();
 	}
