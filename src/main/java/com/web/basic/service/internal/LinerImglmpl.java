a056d8306295d30a89d93e1ade11a1acff794764
hjj569090898 <569090898@qq.com>
2020-12-01 16:57:18 +0800

    铁三角增加校验

--- src/main/java/com/web/basic/service/internal/LinerImglmpl.java
+++ src/main/java/com/web/basic/service/internal/LinerImglmpl.java
@@ -95,7 +95,14 @@ public class LinerImglmpl extends ReportPrcUtils implements LinerImgService {
     public ApiResponseResult edit(LinerImg linerImg) throws Exception {
         LinerImg o = linerImgDao.findById((long) linerImg.getId());
         if(o == null){
-            return ApiResponseResult.failure("该线体不存在！");
+            return ApiResponseResult.failure("该组长铁三角不存在！");
+        }
+        //如果产线或组长修改了
+        if(!o.getEmpIdLiner().equals(linerImg.getEmpIdLiner())||!o.getLineId().equals(linerImg.getLineId())) {
+            if ((linerImgDao.countByLineIdAndEmpIdLinerAndEnabledAndDelFlag(linerImg.getLineId(),
+                    linerImg.getEmpIdLiner(), linerImg.getEnabled(), 0)) > 0) {
+                return ApiResponseResult.failure("组长/产线只能唯一");
+            }
         }
         o.setLastupdateDate(new Date());
         o.setLastupdateBy(UserUtil.getSessionUser().getId());
