d7f6b6cf8b6ded6e74ac2c02c96969e4013ae4d2
fuyuanxiu <fuyuanxiu@126.com>
2020-12-22 09:44:46 +0800

    fyx-quote-sum

---
+++ src/main/java/com/web/quote/service/internal/QuoteSumlmpl.java
@@ -0,0 +1,98 @@
+package com.web.quote.service.internal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.base.data.ApiResponseResult;
+import com.app.base.data.DataGrid;
+import com.utils.BaseSql;
+import com.web.quote.dao.ProductMaterDao;
+import com.web.quote.entity.ProductMater;
+import com.web.quote.entity.Quote;
+import com.web.quote.service.QuoteSumService;
+
+@Service(value = "QuoteSumService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class QuoteSumlmpl extends BaseSql implements QuoteSumService {
+	
+	@Autowired
+    private ProductMaterDao productMaterDao;
+	
+    /**
+     * 查询列表
+     */
+    @Override
+    @Transactional
+    public ApiResponseResult getList(String keyword,PageRequest pageRequest) throws Exception {
+    	String sql = "select distinct p.id,p.bs_Code,p.bs_Type,p.bs_Status,p.bs_Finish_Time,p.bs_Remarks,p.bs_Prod,"
+				+ "p.bs_Similar_Prod,p.bs_Dev_Type,p.bs_Prod_Type,p.bs_Cust_Name,decode(p.bs_end_time3,null,'0','1') col from "+Quote.TABLE_NAME+" p "
+						+ " where p.del_flag=0 and p.bs_step>3 ";
+		if (StringUtils.isNotEmpty(keyword)) {
+			/*sql += "  and INSTR((p.line_No || p.line_Name || p.liner_Code || p.liner_Name ),  '"
+					+ keyword + "') > 0 ";*/
+		}
+		int pn = pageRequest.getPageNumber() + 1;
+		String sql_page = "SELECT * FROM  (  SELECT A.*, ROWNUM RN  FROM ( " + sql + " ) A  WHERE ROWNUM <= ("
+				+ pn + ")*" + pageRequest.getPageSize() + "  )  WHERE RN > (" + pageRequest.getPageNumber() + ")*"
+				+ pageRequest.getPageSize() + " ";
+		
+		Map<String, Object> param = new HashMap<String, Object>();
+		
+		List<Object[]>  list = createSQLQuery(sql_page, param);
+		long count = createSQLQuery(sql, param, null).size();
+		
+		List<Map<String, Object>> list_new = new ArrayList<Map<String, Object>>();
+		for (int i=0;i<list.size();i++) {
+			Object[] object=(Object[]) list.get(i);	
+			Map<String, Object> map1 = new HashMap<>();
+			map1.put("id", object[0]);
+			map1.put("bsCode", object[1]);
+			map1.put("bsType", object[2]);
+			//map1.put("bsStatus", object[3]);
+			map1.put("bsFinishTime", object[4]);
+			map1.put("bsRemarks", object[5]);
+			map1.put("bsProd", object[6]);
+			map1.put("bsSimilarProd", object[7]);
+			map1.put("bsDevType", object[8]);
+			map1.put("bsProdType", object[9]);
+			map1.put("bsCustName", object[10]);
+			map1.put("bsStatus", object[11]);
+			
+			list_new.add(map1);
+		}
+		
+		
+		return ApiResponseResult.success().data(DataGrid.create(list_new, (int) count,
+				pageRequest.getPageNumber() + 1, pageRequest.getPageSize())); 
+    }
+
+	@Override
+	public ApiResponseResult getQuoteList(String keyword, String quoteId, PageRequest pageRequest) throws Exception {
+		// TODO Auto-generated method stub
+		
+		String hql = "select p.* from "+ProductMater.TABLE_NAME+" p where p.del_flag=0 and p.pk_quote="+quoteId;
+		
+		int pn = pageRequest.getPageNumber() + 1;
+		String sql = "SELECT * FROM  (  SELECT A.*, ROWNUM RN  FROM ( " + hql + " ) A  WHERE ROWNUM <= ("
+				+ pn + ")*" + pageRequest.getPageSize() + "  )  WHERE RN > (" + pageRequest.getPageNumber() + ")*"
+				+ pageRequest.getPageSize() + " ";
+		
+		Map<String, Object> param = new HashMap<String, Object>();
+		
+		//List<Map<String, Object>> list = super.findBySql(sql, param);
+		List<ProductMater> list = createSQLQuery(sql, param, ProductMater.class);
+		long count = createSQLQuery(hql, param, null).size();
+		
+		return ApiResponseResult.success().data(DataGrid.create(list, (int) count,
+				pageRequest.getPageNumber() + 1, pageRequest.getPageSize())); 
+	}
+}
