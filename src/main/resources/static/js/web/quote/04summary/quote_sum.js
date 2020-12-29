974b75df78430e6b7d8f6c3d72c881360b8e9101
Clelo-567 <18318912888@163.com>
2020-12-29 16:58:41 +0800

    lst-汇总增加字段数据填置

--- src/main/resources/static/js/web/quote/04summary/quote_sum.js
+++ src/main/resources/static/js/web/quote/04summary/quote_sum.js
@@ -27,7 +27,31 @@ $(function() {
 							"bsFinishTime" : quote_data.bsFinishTime,
 							"bsDevType" : quote_data.bsDevType,
 							"bsCustName" : quote_data.bsCustName,
-							"bsRemarks" : quote_data.bsRemarks
+							"bsRemarks" : quote_data.bsRemarks,
+							
+							"cl_hardware" : quoteDetail.data.cl_hardware,
+							"cl_molding" : quoteDetail.data.cl_molding,
+							"cl_surface" : quoteDetail.data.cl_surface,
+							"cl_packag" : quoteDetail.data.cl_packag,
+							"lh_hardware" : quoteDetail.data.lh_hardware,
+							"lh_molding" : quoteDetail.data.lh_molding,
+							"lh_surface" : quoteDetail.data.lh_surface,
+							"lh_packag" : quoteDetail.data.lh_packag,
+							
+							"lw_hardware" : quoteDetail.data.lw_hardware,
+							"lw_molding" : quoteDetail.data.lw_molding,
+							"lw_surface" : quoteDetail.data.lw_surface,
+							"lw_packag" : quoteDetail.data.lw_packag,
+							"hardware_all" : quoteDetail.data.hardware_all,
+							"molding_all" : quoteDetail.data.molding_all,
+							"surface_all" : quoteDetail.data.surface_all,
+							"packag_all" : quoteDetail.data.packag_all,
+							
+							"wx_all" : quoteDetail.data.wx_all,
+							"hou_loss_all" : quoteDetail.data.hou_loss_all,
+							"mould_all" : quoteDetail.data.mould_all,
+							"gl" : quoteDetail.data.gl,
+							
 						});
 						layui.form.render('select');
 					});
