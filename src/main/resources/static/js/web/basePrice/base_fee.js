410f14605fcb880a5b75a2f66ebb7b7f8e8d0bce
hjj569090898 <569090898@qq.com>
2021-01-19 15:05:05 +0800

    人工制费，制造部工艺机台选择

--- src/main/resources/static/js/web/basePrice/base_fee.js
+++ src/main/resources/static/js/web/basePrice/base_fee.js
@@ -30,10 +30,10 @@ $(function() {
 								}
 							},
 							cols : [ [ {type : 'numbers'}, 
-							           {field : 'enabled',title : '有效状态',templet : '#statusTpl',width : 95}, 
+							           // {field : 'enabled',title : '有效状态',templet : '#statusTpl',width : 95},
 							           {field : 'workcenter',title : '工作中心',width : 100}, 
 							           {field : 'procName',title : '工序',width : 100},
-							           {field : 'mhType',title : '机台类型',width : 100}, 
+							           {field : 'mhType',title : '机台类型',width : 160},
 							           {field : 'feeLh',title : '人工费率（元/小时）',width : 150}, 
 							           {field : 'feeMh',title : '制费费率（元/小时）',width : 150}, 
 							           {field : 'createBy',title : '创建人',width : 80}, 
@@ -117,76 +117,76 @@ $(function() {
 							}
 						});
 						// 工作中心列表
-						cTableSelect = tableSelect.render({
-							elem : '#workcenterName',
-							searchKey : 'keyword',
-							checkedKey : 'id',
-							searchPlaceholder : '试着搜索',
-							table : {
-								// width : 220,
-								url : context
-										+ '/basePrice/baseFee/getWorkCenterList',
-								method : 'get',
-
-								cols : [ [ {
-									type : 'radio'
-								},// 多选 radio
-								, {
-									field : 'ID',
-									title : 'ID',
-									width : 0,
-									hide : true
-								},
-
-								{
-									field : 'WORKCENTER_CODE',
-									title : '工作中心编码',
-
-								},
-
-								{
-									field : 'WORKCENTER_NAME',
-									title : '工作中心',
-								}
-
-								] ],
-								page : true,
-								request : {
-									pageName : 'page' // 页码的参数名称，默认：page
-									,
-									limitName : 'rows' // 每页数据量的参数名，默认：limit
-								},
-								parseData : function(res) {
-									if (!res.result) {
-										// 可进行数据操作
-										return {
-											"count" : 0,
-											"msg" : res.msg,
-											"data" : [],
-											"code" : res.status
-										// code值为200表示成功
-										}
-									}
-									return {
-										"count" : res.data.Total,
-										"msg" : res.msg,
-										"data" : res.data.List,
-										"code" : res.status
-									// code值为200表示成功
-									}
-								},
-							},
-							done : function(elem, data) {
-								// 选择完后的回调，包含2个返回值
-								// elem:返回之前input对象；data:表格返回的选中的数据 []
-								var da = data.data;
-								form.val("itemForm", {
-									"workcenterId" : da[0].ID,
-									"workcenterName" : da[0].WORKCENTER_NAME,
-								});
-								form.render();// 重新渲染
-							}
-						});
+						// cTableSelect = tableSelect.render({
+						// 	elem : '#workcenterName',
+						// 	searchKey : 'keyword',
+						// 	checkedKey : 'id',
+						// 	searchPlaceholder : '试着搜索',
+						// 	table : {
+						// 		// width : 220,
+						// 		url : context
+						// 				+ '/basePrice/baseFee/getWorkCenterList',
+						// 		method : 'get',
+						//
+						// 		cols : [ [ {
+						// 			type : 'radio'
+						// 		},// 多选 radio
+						// 		, {
+						// 			field : 'ID',
+						// 			title : 'ID',
+						// 			width : 0,
+						// 			hide : true
+						// 		},
+						//
+						// 		{
+						// 			field : 'WORKCENTER_CODE',
+						// 			title : '工作中心编码',
+						//
+						// 		},
+						//
+						// 		{
+						// 			field : 'WORKCENTER_NAME',
+						// 			title : '工作中心',
+						// 		}
+						//
+						// 		] ],
+						// 		page : true,
+						// 		request : {
+						// 			pageName : 'page' // 页码的参数名称，默认：page
+						// 			,
+						// 			limitName : 'rows' // 每页数据量的参数名，默认：limit
+						// 		},
+						// 		parseData : function(res) {
+						// 			if (!res.result) {
+						// 				// 可进行数据操作
+						// 				return {
+						// 					"count" : 0,
+						// 					"msg" : res.msg,
+						// 					"data" : [],
+						// 					"code" : res.status
+						// 				// code值为200表示成功
+						// 				}
+						// 			}
+						// 			return {
+						// 				"count" : res.data.Total,
+						// 				"msg" : res.msg,
+						// 				"data" : res.data.List,
+						// 				"code" : res.status
+						// 			// code值为200表示成功
+						// 			}
+						// 		},
+						// 	},
+						// 	done : function(elem, data) {
+						// 		// 选择完后的回调，包含2个返回值
+						// 		// elem:返回之前input对象；data:表格返回的选中的数据 []
+						// 		var da = data.data;
+						// 		form.val("itemForm", {
+						// 			"workcenterId" : da[0].ID,
+						// 			"workcenterName" : da[0].WORKCENTER_NAME,
+						// 		});
+						// 		form.render();// 重新渲染
+						// 	}
+						// });
 						// 机台类型列表
 						typeTableSelect = tableSelect.render({
 							elem : '#mhType',
@@ -195,31 +195,14 @@ $(function() {
 							searchPlaceholder : '试着搜索',
 							table : {
 								// width : 220,
-								url : context
-										+ '/basePrice/baseFee/getType',
+								url : context + '/basePrice/modelType/getList',
 								method : 'get',
-
-								cols : [ [ {
-									type : 'radio'
-								},// 多选 radio
-								, {
-									field : 'ID',
-									title : 'ID',
-									width : 0,
-									hide : true
-								},
-
-								{
-									field : 'SUB_CODE',
-									title : '参数编码',
-
-								},
-
-								{
-									field : 'SUB_NAME',
-									title : '参数名称',
-								}
-
+								cols : [ [ {type : 'radio'},
+									{field : 'ID', title : 'ID', width : 0, hide : true},
+									{field : 'modelCode', title : '机台编码'},
+									{field : 'modelName', title : '机台描述'},
+									{field : 'workCenterCode', title : '工作中心编码'},
+									{field : 'workCenterName', title : '工作中心名称'},
 								] ],
 								page : true,
 								request : {
@@ -239,11 +222,11 @@ $(function() {
 										}
 									}
 									return {
-										"count" : res.data.Total,
+										"count" : res.data.total,
 										"msg" : res.msg,
-										"data" : res.data.List,
+										"data" : res.data.rows,
 										"code" : res.status
-									// code值为200表示成功
+										// code值为200表示成功
 									}
 								},
 							},
@@ -252,7 +235,9 @@ $(function() {
 								// elem:返回之前input对象；data:表格返回的选中的数据 []
 								var da = data.data;
 								form.val("itemForm", {
-									"mhType" : da[0].SUB_NAME,
+									"mhType" : da[0].modelName,
+									"workcenterId":da[0].workCenterId,
+									"workcenterName":da[0].workCenterName,
 								});
 								form.render();// 重新渲染
 							}
